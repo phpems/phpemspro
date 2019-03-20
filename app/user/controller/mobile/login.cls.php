f04c83e29014a72b7185cf350d126eb1c78a5251
redrangon <redrangon@tom.com>
2019-03-20 23:28:06 +0800

    注册bug

--- app/user/controller/mobile/login.cls.php
+++ app/user/controller/mobile/login.cls.php
@@ -155,7 +155,7 @@ class login
                 );
                 exit(json_encode($message));
             }
-            $fargs = array('username' => $username,'userphone' => $phone,'usergroupid' => $defaultgroup['groupcode'],'userpassword' => $args['userpassword'],'useremail' => $email);
+            $fargs = array('username' => $username,'userphone' => $phone,'usergroupcode' => $defaultgroup['groupcode'],'userpassword' => $args['userpassword'],'useremail' => $email);
             $id = users::addUser($fargs);
             \session::getInstance()->setSessionUser(array('sessionuserid'=>$id,'sessionpassword'=>md5($args['userpassword']),'sessionip'=>\route::getClientIp(),'sessiongroupid'=>$defaultgroup['groupid'],'sessionlogintime'=>TIME,'sessionusername'=>$username));
             $message = array(
