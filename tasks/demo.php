c5761f42fa66fafd47d1255d4a84ad42a39e9c25
redrangon <redrangon@tom.com>
2019-03-31 16:50:32 +0800

    任务

---
+++ tasks/demo.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Created on 2013-12-26
+ * 用于定时任务调起
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+set_time_limit(0);
+define('PEP_PATH',dirname(dirname(__FILE__)));
+include PEP_PATH.'/public/lib/base.cls.php';
+class app
+{
+    static function display()
+    {
+        //task codes
+    }
+}
+app::display();
